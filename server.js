58901cd148e62e08b3f0993b9ec20d65039c0262
Selin Huang <selin.hyx@gmail.com>
2019-07-27 00:23:40 -0400

    modified ejs files to include index.ejs file in other files

--- server.js
+++ server.js
@@ -47,9 +47,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("events_public");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
