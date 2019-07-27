c5a8b8721fce2a99b3ee00f3fa1c253610b1865c
Anchen Huang <acmhuang@gmail.com>
2019-07-27 20:16:13 +0000

    ADDED form action for events admin slig and method for post. For admin page

--- routes/eventsAdmin.js
+++ routes/eventsAdmin.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = (db) => {
+module.exports = db => {
   router.get("/", (req, res) => {
     res.render("../views/events_admin");
   });
