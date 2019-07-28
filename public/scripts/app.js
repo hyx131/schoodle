bcc0cf6669f1c07c5973f412e40bf13d5842e72a
Selin Huang <selin.hyx@gmail.com>
2019-07-28 19:30:33 -0400

    started functions to create html template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,14 @@
 //     }
 //   });;
 // });
+
+
+const createTimeSlots = function(events) {
+  let $slot = `
+    <p> ${events.date} </P>
+    <form>
+      <input type='text' id='start_time' placeholder='start time'>
+      <input type='text' id='end_time' placeholder='end time'>
+    </form>
+  `
+}
