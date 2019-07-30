584c6bbe69c4b32ddeadbb1bb671420dc1649432
Selin Huang <selin.hyx@gmail.com>
2019-07-29 21:23:10 -0400

    added start-time end-time html section to events_new.ejs with ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,57 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+
+// helper function to generate new time slots:
+const createTimeSlots = function(date) {
+
+  let $slot = `
+  <p>${date}</p>
+  <label for='start_time'>Start Time:</label>
+  <input
+  type='text'
+  id='start_time'
+  name="startTime"
+  placeholder='start time'
+  />
+  <label for='end_time'>End Time:</label>
+  <input
+  type='text'
+  id='end_time'
+  name="endTime"
+  placeholder='end time'
+  />`;
+
+  return $slot;
+};
+
+
+// helper function to display month properly:
+const getMonth = function(month) {
+  let arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  let m = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (i === month) {
+      m += arr[i];
+    }
+  }
+  return m;
+};
+
+
+// clicking 'onChange' event on calendar then render the 'time_slots' section:
+$(document).ready(function() {
+  console.log("zzzzzz");
+  $("#pick_date").flatpickr({
+    inline: true,
+    onChange: function(selectedDates, dateStr, instance) {
+      const date = selectedDates[0].getDate();
+      const month = selectedDates[0].getMonth(); //January is 0 not 1
+      const year = selectedDates[0].getFullYear();
+
+      const dateString = getMonth(month) + "/" + date + "/" + year;
+
+      $('.time_slots').append(createTimeSlots(dateString));
+    }
+  })
+});
+
 
 
